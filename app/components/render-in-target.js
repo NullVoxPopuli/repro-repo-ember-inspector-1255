b711064c574d79ee66232185d9bc5f14563266bd
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-10-26 20:44:48 -0400

    update example

--- app/components/render-in-target.js
+++ app/components/render-in-target.js
@@ -1,9 +1,12 @@
 import Component from '@glimmer/component';
+import { assert } from '@ember/debug';
 
 export default class extends Component {
   constructor(owner, args) {
     super(owner, args);
 
-    this.element = document.querySelector('#target');
+    this.element = document.querySelector(`#${args.id}`);
+
+    assert(`element not found for id: ${args.id}`, this.element);
   }
 }
