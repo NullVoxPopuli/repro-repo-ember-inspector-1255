4ff6415b09dd711035420eee0afd92fb7521971d
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-10-26 20:51:02 -0400

    upgrade ember

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'in-element-ember-inspector-reproduction/app';
+import config from 'in-element-ember-inspector-reproduction/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
